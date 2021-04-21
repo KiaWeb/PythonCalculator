321636f942bb33245cbb6ec1774e4352005af4e9
KiaWeb <82962111+KiaWeb@users.noreply.github.com>
2021-04-21 11:12:12 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+import time
+from time import sleep
+e = input("a, s, m, d, or fd: ")
+wt = "Please wait 5 seconds to restart."
+def getn(a1, a2, s):
+  if s == "a":
+     a1 = int(a1)
+     a2 = int(a2)
+     b1 = a1 + a2
+     c1 = str(b1)
+     print("Answer: " + c1)
+     time.sleep(1)
+     print(wt)
+     time.sleep(5)
+  if s == "s":
+      a1 = int(a1)
+      a2 = int(a2)
+      b1 = a1 - a2
+      c1 = str(b1)
+      print("Answer: " + c1)
+      time.sleep(1)
+      print(wt)
+      time.sleep(5)
+  if s == "m":
+      a1 = int(a1)
+      a2 = int(a2)
+      b1 = a1 * a2
+      c1 = str(b1)
+      print("Answer: " + c1)
+      time.sleep(1)
+      print(wt)
+      time.sleep(5)
+  if s == "d":
+      a1 = int(a1)
+      a2 = int(a2)
+      b1 = a1 / a2
+      c1 = str(b1)
+      print("Answer: " + c1)
+      time.sleep(1)
+      print(wt)
+      time.sleep(5)
+  if s == "fd":
+        a1 = int(a1)
+        a2 = int(a2)
+        b1 = a1 // a2
+        c1 = str(b1)
+        print("Answer: " + c1)
+        time.sleep(1)
+        print(wt)
+        time.sleep(5)
+def run(t):
+  ask = input("What is the 1st number? ")
+  ask2 = input("What is the 2nd number? ")
+  getn(ask, ask2, t)
+run(e)
+while True:
+  run(input("a, s, m, d, or fd: "))
